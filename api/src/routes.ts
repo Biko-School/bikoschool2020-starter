2698aaa7aae789b10bef922c114380c93168110e
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-20 20:29:58 +0200

    Pasamos a el test de los 50 memes pero obteniendo los memes desde db con lowdb

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,7 +1,15 @@
 import Router from 'express'
+import low from 'lowdb/lib/fp'
+import FileSync from 'lowdb/adapters/FileSync'
+import { concat, find, sortBy, take, random } from 'lodash/fp'
 
 export const routes = Router()
 
+const adapter = new FileSync('./db/db.json')
+const db = low(adapter)
+const defaultValue = []
+const memes = db('memes',defaultValue)
+
 routes.get('/memes',function(req,res){
-    res.status(200).send(new Array(50))
+    res.status(200).send(memes([take(50)]))
 })
