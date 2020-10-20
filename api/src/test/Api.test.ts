5ca8a71231df5ef910d77b7a0b9229c59d032fd1
Miguel Echávarri <miguelepmrs@gmail.com>
2020-10-20 18:57:32 +0200

    Test en rojo comprobar que /api/memes devuelve un array

--- api/src/test/Api.test.ts
+++ api/src/test/Api.test.ts
@@ -1,8 +1,21 @@
 import request from 'supertest'
 import app from '../app'
 
+const apiPath = '/api'
+const rutaMemes = apiPath + '/memes'
+const HTTP_OK = 200
+
 describe('GET /api/memes', function () {
   it('responds with 200', function (done) {
-    request(app).get('/api/memes').expect(200, done)
+    request(app).get(rutaMemes).expect(HTTP_OK, done)
+  })
+  it('responds with array', function (done) {
+    request(app)
+      .get(rutaMemes)
+      .expect(HTTP_OK)
+      .then((response) => {
+        expect(response.body).toBeInstanceOf(Array)
+        done()
+      })
   })
 })
