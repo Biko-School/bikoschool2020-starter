59093f7ae61ee5a77f33481db2686601e091b848
Miguel Echávarri <miguelepmrs@gmail.com>
2020-10-20 19:04:21 +0200

    Test en verde repuesta en array

--- api/src/app.ts
+++ api/src/app.ts
@@ -16,6 +16,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/api/memes', (req: Request, res: Response) => {
-  res.json({ data: 'index!' })
+  res.json([{ data: 'index!' }])
 })
 export default app
