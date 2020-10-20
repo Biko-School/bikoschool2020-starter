b1e2124320185d27d169c97ea83c88781b8c2549
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-20 19:16:08 +0200

    pasado las dos primeras pruebas y comenzando a hacer las rutas

---
+++ api/src/app.test.ts
@@ -0,0 +1,22 @@
+import app from './app'
+import request from 'supertest'
+import { response } from 'express'
+
+describe('/api/memes', () => {
+  it('existe el endpoint', function(done){
+    request(app)
+    .get('/api/memes')
+    .expect(200,done)
+  })
+
+  it('el endpoint devuelve una lista', function(done){
+    request(app)
+    .get('/api/memes')
+    .expect(200).then(response =>{
+      expect(response.body).toBeInstanceOf(Array)
+      done()
+    })
+  })
+
+})
+
