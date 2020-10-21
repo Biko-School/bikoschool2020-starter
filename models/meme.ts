f8a76e3dba35b55ee5d1af98a280dd05e2e17131
Cilveti <icilveti@rockthesport.com>
2020-10-21 13:22:58 +0200

    clase meme

---
+++ models/meme.ts
@@ -0,0 +1,34 @@
+declare module namespace {
+
+    export interface Original {
+        width: string;
+        height: string;
+        url: string;
+    }
+
+    export interface Small {
+        width: string;
+        height: string;
+        url: string;
+    }
+
+    export interface Images {
+        original: Original;
+        small: Small;
+    }
+
+    export interface Meme {
+        id: string;
+        type: string;
+        slug: string;
+        giphyUrl: string;
+        title: string;
+        source_tld: string;
+        source_post_url: string;
+        import_datetime: string;
+        username: string;
+        images: Images;
+        tags: string[];
+    }
+
+}
