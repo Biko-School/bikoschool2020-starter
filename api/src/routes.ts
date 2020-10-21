c941e4d9e784266c2419942bfe33f3e60a320e55
xabi2020 <xfuentes@hotmail.com>
2020-10-21 12:10:47 +0200

    Devuelve un listado de 50 memes

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,7 +1,14 @@
 import express from 'express'
+import low from 'lowdb'
+import FileSync from 'lowdb/adapters/FileSync'
+import { DatabaseSchema } from './DatabaseSchema'
 
 export const router = express.Router()
 
 router.get('/memes', (req, res) => {
-  res.status(200).json([])
+  const adapter = new FileSync<DatabaseSchema>('./db/db.json')
+  const db = low(adapter)
+
+  const memes = db.get('memes').take(50).value()
+  res.status(200).json(memes)
 })
