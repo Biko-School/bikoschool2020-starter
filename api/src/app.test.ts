a351451320842d1a259b302da7baf2b7a5381b1f
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-23 19:12:46 +0200

    Hemos modificado la clase de test para que se adapte al nuevo sistema de llamada a api y tenemos los test en verde

--- api/src/app.test.ts
+++ api/src/app.test.ts
@@ -1,15 +1,26 @@
 import { createApp } from './app'
 import request from 'supertest'
+import Memory from 'lowdb/adapters/Memory'
+import { DatabaseSchema } from 'DatabaseSchema'
+import low from 'lowdb'
 import { response } from 'express'
-
+import dbTest from "./fixtures/db.json"
 describe('/api/memes', () => {
   it('existe el endpoint', function(done){
+    const adapter = new Memory<DatabaseSchema>('')
+    const db =  low(adapter)
+    db.defaults({memes: []}).write()
+    const app = createApp(db)
     request(app)
     .get('/api/memes')
     .expect(200,done)
   })
 
-  it('el endpoint devuelve una lista', function(done){
+  it('el endpoint devuelve una lista', (done)=>{
+    const adapter = new Memory<DatabaseSchema>("")
+    const db = low(adapter)
+    db.defaults({memes: []}).write()
+    const app = createApp(db)
     request(app)
     .get('/api/memes')
     .expect(200).then(response =>{
@@ -18,7 +29,11 @@ describe('/api/memes', () => {
     })
   })
 
-  it('el endpoint devuelve una lista', function(done){
+  it('el endpoint devuelve una lista de 50 memes', function(done){
+    const adapter = new Memory<DatabaseSchema>("")
+    const db = low(adapter)
+    db.defaults(dbTest).write()
+    const app = createApp(db)
     request(app)
     .get('/api/memes')
     .expect(200).then(response =>{
@@ -26,6 +41,5 @@ describe('/api/memes', () => {
       done()
     })
   })
-
 })
 
