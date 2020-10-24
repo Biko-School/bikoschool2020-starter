c2b4561bbb088d10db8ed19d2a00a83c702440b0
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-24 13:20:09 +0200

    En routes hemos cambiado a una constante el numero de memes que te tiene que devolver para que luego sea parametrizable

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -7,9 +7,10 @@ export function createRoutes(db:LowdbSync<DatabaseSchema>){
     const routes = Router()
 
     routes.get('/memes',function(req,res){
+        const numeroMememes = 3
         const memes = db.get("memes") 
                         .sortBy('import_datetime').reverse() 
-                        .take(3)
+                        .take(numeroMememes)
                         .value() 
         res.status(200).json(memes)
     })
