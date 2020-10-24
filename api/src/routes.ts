4cfe2b2c6c3332021294f40f37f73e3d4e4493d2
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-24 13:18:10 +0200

    hemos unificado los dos test anteriores pero para que se hagan bajo un contexto controlado

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -9,7 +9,7 @@ export function createRoutes(db:LowdbSync<DatabaseSchema>){
     routes.get('/memes',function(req,res){
         const memes = db.get("memes") 
                         .sortBy('import_datetime').reverse() 
-                        .take(50)
+                        .take(3)
                         .value() 
         res.status(200).json(memes)
     })
--- api/src/test/app.test.ts
+++ api/src/test/app.test.ts
@@ -8,29 +8,22 @@ import { aMeme } from "./builders/memeBuilder"
 import { aDatabase } from "./builders/dataBaseBuilder"
 
 describe('/api/memes', () => {
-  it('devuelve una lista de 50 memes', function(done){
-    const app = implementApp(dbTest)
-    request(app)
-    .get('/api/memes')
-    .expect(200).then(response =>{
-      expect(response.body).toHaveLength(50)
-      done()
-    })
-  })
-
-  it('devuelve una lista de 50 memes ordenados por su fecha de creación en orden descendente', function(done){
+  it('devuelve los memes más recientes ordenados por fecha decreciente', function(done){
     const memeUno = aMeme().withDate("2020-08-20 02:24:22").build()
     const memeDos = aMeme().withDate("2020-08-26 22:51:59").build()
     const memeTres = aMeme().withDate("2020-08-17 19:05:41").build()
-    const arrayMemes = [memeUno, memeDos, memeTres]
+    const memeCuatro = aMeme().withDate("2020-09-20 02:24:22").build()
+    const memeCinco = aMeme().withDate("2020-12-26 22:51:59").build()
+    const memeSeis = aMeme().withDate("2020-01-17 19:05:41").build()
+    const arrayMemes = [memeUno, memeDos, memeTres, memeCuatro, memeCinco, memeSeis]
     const db = aDatabase().withMemes(arrayMemes).build()
     const app = implementApp(db)
-    request(app)
-    .get('/api/memes')
-    .expect(200).then(response => {
-      expect(response.body[0]).toHaveProperty('import_datetime',"2020-08-26 22:51:59")
-      expect(response.body[1]).toHaveProperty('import_datetime',"2020-08-20 02:24:22")
-      expect(response.body[2]).toHaveProperty('import_datetime',"2020-08-17 19:05:41")
+    request(app).get('/api/memes')
+    .expect(200).then(response =>{
+      expect(response.body).toHaveLength(3)
+      expect(response.body[0]).toHaveProperty('import_datetime', '2020-12-26 22:51:59')
+      expect(response.body[1]).toHaveProperty('import_datetime', '2020-09-20 02:24:22')
+      expect(response.body[2]).toHaveProperty('import_datetime', '2020-08-26 22:51:59')
       done()
     })
   })
