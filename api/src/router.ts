b75971af8f98122de8313a017d37c67236f6d558
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-10-24 13:23:34 +0200

    refactor: extract methods from reaterRouter

--- api/src/router.ts
+++ api/src/router.ts
@@ -7,30 +7,31 @@ import { Meme } from './Meme'
 export const createRouter = (db: Lowdb.LowdbSync<DatabaseSchema>) => {
   const router = express.Router()
 
-  function getMemes() {
-    return db.get('memes').sortBy('import_datetime').reverse()
-  }
-  function mapMemesDatabaseToMemes(memesDatabase: MemeDatabase[]): Meme[] {
-    return memesDatabase.map((meme) => ({
-      id: meme.id,
-      title: meme.title,
-      url: meme.images.small.url,
-      creationDate: meme.import_datetime,
-    }))
-  }
-
   router.get('/memes', (req, res) => {
     let databaseMemes: MemeDatabase[]
     if (req.query.search) {
-      databaseMemes = getMemes()
+      databaseMemes = getMemes(db)
         .filter({ tags: [req.query.search] })
         .value()
     } else {
-      databaseMemes = getMemes().take(50).value()
+      databaseMemes = getMemes(db).take(50).value()
     }
 
     const memes: Meme[] = mapMemesDatabaseToMemes(databaseMemes)
     res.status(200).json({ memes })
   })
   return router
+}
+
+function getMemes(db: Lowdb.LowdbSync<DatabaseSchema>) {
+  return db.get('memes').sortBy('import_datetime').reverse()
+}
+
+function mapMemesDatabaseToMemes(memesDatabase: MemeDatabase[]): Meme[] {
+  return memesDatabase.map((meme) => ({
+    id: meme.id,
+    title: meme.title,
+    url: meme.images.small.url,
+    creationDate: meme.import_datetime,
+  }))
 }
