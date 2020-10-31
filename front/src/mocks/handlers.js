ac6596fa5b0b014d606d19465c7af4cb2b3404dd
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-10-31 12:41:04 +0100

    Quitados comentarios innecesarios del handlers.js

--- front/src/mocks/handlers.js
+++ front/src/mocks/handlers.js
@@ -1,9 +1,7 @@
-// src/mocks/handlers.js
 import { rest } from 'msw'
 import memes from '../test.json'
 
 export const handlers = [
-  // Handles a GET /user request
   rest.get('/memes', (req,res,ctx) =>{
     return res(
         ctx.status(200),
