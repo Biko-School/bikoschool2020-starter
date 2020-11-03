daf39b598a891ba290931adfc638e7a7d12ee3d7
user <user@PRESTAMOA01.local>
2020-11-03 10:58:16 +0100

    Add: GlobalStyles

---
+++ front/src/ui/theme/GlobalStyles/GlobalStyles.ts
@@ -0,0 +1,28 @@
+import { createGlobalStyle } from 'styled-components'
+import { colors, font } from '../theme'
+
+import { reboot } from './reboot'
+import { reset } from './reset'
+
+export const GlobalStyles = createGlobalStyle`
+${reset};
+${reboot};
+
+  body {
+    font-family: 'Open Sans', sans-serif;
+    ${font.base()};
+
+    background-image: linear-gradient(
+    150deg,
+    ${colors.blue} 0%,
+    ${colors.pink} 46%,
+    ${colors.yellow} 100%
+    );
+    background-position: center; 
+    background-repeat: no-repeat; 
+    background-size: cover;
+
+    color: ${colors.white};
+  }
+
+`
