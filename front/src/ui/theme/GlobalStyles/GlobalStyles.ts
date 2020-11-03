03c4c2ac806914c08dc70b05a26682955ec0ec4a
user <user@PRESTAMOA01.local>
2020-11-03 11:53:06 +0100

    Modifico el background para que coja todo el body

--- front/src/ui/theme/GlobalStyles/GlobalStyles.ts
+++ front/src/ui/theme/GlobalStyles/GlobalStyles.ts
@@ -21,6 +21,7 @@ ${reboot};
     background-position: center; 
     background-repeat: no-repeat; 
     background-size: cover;
+    background-attachment: fixed;
 
     color: ${colors.white};
   }
