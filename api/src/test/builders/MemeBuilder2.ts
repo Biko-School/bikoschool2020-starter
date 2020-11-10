700def2a7abd2f8a483c53ab2034ebea2bacf603
user <user@PRESTAMOA01.local>
2020-11-10 10:28:48 +0100

    Creo un nuevo builder con API fluida para simplificar los nuevos test

---
+++ api/src/test/builders/MemeBuilder2.ts
@@ -0,0 +1,39 @@
+import { MemeSchema } from 'databaseSchema'
+
+export function aMeme(id: string) {
+  let defaults : MemeSchema = {
+    id,
+    title: 'irrelevant',
+    images: {
+      small: {
+        width: '200',
+        height: '100',
+        url: 'irrelevant',
+      },
+      original: {
+        width: '200',
+        height: '100',
+        url: 'irrelevant',
+      },
+    },
+    tags: ['#foo'],
+    
+    import_datetime: 'irrelevant',
+    username: 'irrelevant',
+    type: 'irrelevant',
+    slug: 'irrelevant',
+    giphyUrl: 'irrelevant',
+    source_tld: 'irrelevant',
+    source_post_url: 'irrelevant'
+  }
+
+  return {
+    withTags(tags: string[]) {
+      defaults.tags = tags
+      return this
+    },
+    build() {
+      return defaults
+    },
+  }
+}
