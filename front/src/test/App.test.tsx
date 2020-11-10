76f46cb00220e32d3c6af9608c0f6bba4ba4fdb2
user <user@PRESTAMOA01.local>
2020-11-10 13:02:46 +0100

    Paso los tests del front a la carpeta test

--- front/src/test/App.test.tsx
+++ front/src/test/App.test.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import { fireEvent, render, screen } from '@testing-library/react'
-import App from './App'
-import memes from './fixtures/memes.json'
-import { server } from './mocks/server'
+import App from '../App'
+import memes from '../fixtures/memes.json'
+import { server } from '../mocks/server'
 import { rest } from 'msw'
 
 const errorMessage500 = 'Se ha producido un error'
