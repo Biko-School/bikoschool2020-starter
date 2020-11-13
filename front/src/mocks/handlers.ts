6f79618560c8753606b716abb1a0b2e358ade467
Leyre <leyre.garralda@gmail.com>
2020-11-13 17:22:55 +0100

    fix: añade handler de comit "devuelve busqueda"

--- front/src/mocks/handlers.ts
+++ front/src/mocks/handlers.ts
@@ -4,7 +4,12 @@ import Memes from '../memes.json'
 
 export const handlers = [
   rest.get('http://localhost:3000/api/memes', (req, res, ctx) => {
-    // Return a mocked memes list
+    const search = req.url.searchParams.get('search')
+
+    if (search) {
+      return res(ctx.status(200), ctx.json([Memes[0], Memes[1]]))
+    }
+    //Return a mocked memes list
     return res(ctx.status(200), ctx.json(Memes))
   }),
 ]
