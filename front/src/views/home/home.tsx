eed8aec112bd3a96902def7702e88deb57491a57
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-11-14 13:06:37 +0100

    Corregidos punto y coma

--- front/src/views/home/home.tsx
+++ front/src/views/home/home.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Meme } from '../../core/domain/Meme/Meme';
-import { getMemes } from '../../core/domain/Meme/Memes.service'
-import { Buscador } from './_components/Buscador'
-import { MemesList } from './_components/MemesList'
+import React from "react";
+import { Meme } from "../../core/domain/Meme/Meme";
+import { getMemes } from "../../core/domain/Meme/Memes.service";
+import { Buscador } from "./_components/Buscador";
+import { MemesList } from "./_components/MemesList";
 
-export const Home: React.FC=() =>{
-    const [memes,setMemes] = React.useState<Meme[]>([])
+export const Home: React.FC = () => {
+  const [memes, setMemes] = React.useState<Meme[]>([]);
 
-    React.useEffect(() => {
-        getMemes().then(setMemes)
-    },[])
-    return (
-        <>  
-            <Buscador/>
-            <MemesList memes={memes} />
-        </>
-    );
-}
+  React.useEffect(() => {
+    getMemes().then(setMemes);
+  }, []);
+  return (
+    <>
+      <Buscador />
+      <MemesList memes={memes} />
+    </>
+  );
+};
