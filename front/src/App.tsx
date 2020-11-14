3123917efd303233f295a46f986ccca3752f247b
inaki-elizari <inaki.elizari@conasa.es>
2020-11-14 10:17:39 +0100

    P

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -6,10 +6,7 @@ import { RecentMemesList } from './views/MemesList';
 import { SearchBar } from './views/SearchBar';
 
 function App() {
-  let prueba = {
-    jelou: 1,
-    baibai: 2,
-  };
+  let prueba = { jelou: 1, baibai: 2 };
   return (
     <>
       <GlobalStyles />
