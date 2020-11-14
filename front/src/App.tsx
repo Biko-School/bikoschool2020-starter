3ac57dfded0bdbeb516b0bfc9ca9bb5efc49192e
calvo111979 <acalvoinfa10@gmail.com>
2020-11-14 13:07:16 +0100

    intento de romper prettier

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -48,6 +48,8 @@ function App() {
       <GlobalContainer>
         <SecondaryContainer>
           <Header>
+
+            
             <LogoWrapper />
             GUIFAFFINITY
           </Header>
