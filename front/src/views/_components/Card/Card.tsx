503b127c784a47e3c793c4b32b8c6b3e039d8528
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-11-17 19:28:03 +0100

    feat: layout card author and avatar

--- front/src/views/_components/Card/Card.tsx
+++ front/src/views/_components/Card/Card.tsx
@@ -1,11 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
+import { size, fontWeight, font, iconSize, colors } from './../../../ui/theme'
+import { rem } from 'polished'
 
-const Image = styled.img`
-  width: 100%;
-  height: auto;
-  display: block;
-`
 interface Props {
   image: {
     src: string
@@ -18,13 +15,35 @@ interface Props {
 }
 
 export const Card: React.FC<Props> = ({ image, author }) => (
-  <>
+  <Wrapper>
     <Image src={image.src} alt={image.alt} />
     {author && (
-      <div>
-        <div>{author.displayName}</div>
-        <Image src={author.avatarUrl} alt={author.displayName} />
-      </div>
+      <Author>
+        <Avatar src={author.avatarUrl} alt={author.displayName} />
+        {author.displayName}
+      </Author>
     )}
-  </>
+  </Wrapper>
 )
+const Image = styled.img`
+  width: 100%;
+  height: auto;
+  display: block;
+`
+const Wrapper = styled.div`
+  position: relative;
+`
+const Author = styled.div`
+  position: absolute;
+  bottom: ${rem(size.tiny)};
+  left: ${rem(size.tiny)};
+  color: ${colors.white};
+  display: flex;
+  align-items: center;
+  column-gap: ${rem(size.tiny)};
+`
+const Avatar = styled.img`
+  width: ${rem(size.medium)};
+  height: ${rem(size.medium)};
+  object-fit: cover;
+`
