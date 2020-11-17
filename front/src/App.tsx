bf29353970ee490c13621daaff66e30621b0cd66
user <user@PRESTAMOA01.bikowifi>
2020-11-17 19:32:43 +0100

    Mueve a fichero

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { Meme } from './Meme'
-import { getMemesData, getFilteredMemesData } from './services/getMemesData'
 import { GlobalStyles } from './ui/theme/GlobalStyles/GlobalStyles'
 import { Container } from './views/Home/_components/Container'
 import {
@@ -9,56 +7,8 @@ import {
   HeaderLogo,
   AppName,
 } from './views/Home/_components/Header'
-import { SearchBox } from './views/Home/_components/SearchBox'
-import { MemeList } from './views/Home/_components/MemeList/MemeList'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
-const Home: React.FC = () => {
-  const [memesData, setMemesData] = React.useState<Meme[]>([])
-  const [filter, setFilter] = React.useState<string>('')
-  const [error, setError] = React.useState<string | null>(null)
-
-  React.useEffect(() => {
-    getMemesData()
-      .then((data) => {
-        setMemesData(data)
-      })
-      .catch((error) => {
-        setError('Se ha producido un error al obtener los memes mas recientes')
-      })
-  }, [])
-
-  const handleSearch = () => {
-    setError(null)
-    const trimmedFilter = filter.trim().replace(/\s+/g, ' ')
-
-    if (trimmedFilter.length < 3) {
-      setError(
-        'La longitud del termino de busqueda debe ser mayor de 2 caracteres',
-      )
-    }
-
-    getFilteredMemesData(trimmedFilter)
-      .then((data) => {
-        setMemesData(data)
-      })
-      .catch((error) => {
-        setError(
-          'Se ha producido un error al realizar la búsqueda de los memes',
-        )
-      })
-  }
-  return (
-    <>
-      <SearchBox
-        filter={filter}
-        onFilterChanged={setFilter}
-        onSearch={() => handleSearch()}
-      />
-      {error ? <p>{error}</p> : <MemeList memes={memesData} />}
-    </>
-  )
-}
+import { Home } from './Home'
 
 const App: React.FC = () => {
   return (
---
+++ front/src/Home.tsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Meme } from './Meme'
+import { getMemesData, getFilteredMemesData } from './services/getMemesData'
+import { SearchBox } from './views/Home/_components/SearchBox'
+import { MemeList } from './views/Home/_components/MemeList/MemeList'
+
+export const Home: React.FC = () => {
+  const [memesData, setMemesData] = React.useState<Meme[]>([])
+  const [filter, setFilter] = React.useState<string>('')
+  const [error, setError] = React.useState<string | null>(null)
+
+  React.useEffect(() => {
+    getMemesData()
+      .then((data) => {
+        setMemesData(data)
+      })
+      .catch((error) => {
+        setError('Se ha producido un error al obtener los memes mas recientes')
+      })
+  }, [])
+
+  const handleSearch = () => {
+    setError(null)
+    const trimmedFilter = filter.trim().replace(/\s+/g, ' ')
+
+    if (trimmedFilter.length < 3) {
+      setError(
+        'La longitud del termino de busqueda debe ser mayor de 2 caracteres',
+      )
+    }
+
+    getFilteredMemesData(trimmedFilter)
+      .then((data) => {
+        setMemesData(data)
+      })
+      .catch((error) => {
+        setError(
+          'Se ha producido un error al realizar la búsqueda de los memes',
+        )
+      })
+  }
+  return (
+    <>
+      <SearchBox
+        filter={filter}
+        onFilterChanged={setFilter}
+        onSearch={() => handleSearch()}
+      />
+      {error ? <p>{error}</p> : <MemeList memes={memesData} />}
+    </>
+  )
+}
