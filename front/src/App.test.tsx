a0536f0e982984c7093426d67dc8f2e83def9eff
Marcos Galvez <galvez.115053@e.unavarra.es>
2020-11-18 16:52:18 +0100

    Subiendo para Fer

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { render,screen } from '@testing-library/react';
 import App from './App';
 import {memes} from '../src/test.json'
+import userEvent from '@testing-library/user-event';
 
 describe('renders learn react link', () => {
 
@@ -13,6 +14,19 @@ describe('renders learn react link', () => {
       expect(meme).toHaveAttribute("alt",memes[i].title)
       expect(meme).toHaveAttribute("src",memes[i].url)
     }
+  })
+
+  it('devuelve error al pasarle 2 caracteres de búsqueda', function(done){
+    render(<App />)
+
+    const search = screen.getByRole('textbox',{name:"search"})
+    userEvent.type(search,'ho')
+
+    userEvent.click(screen.getByRole('button',{name:'search'}))
+
+    
+           
+    expect(screen.getByText('El texto de búsqueda necesita ser mayor que dos caracteres')).toBeInTheDocument()
   })
 })
 
