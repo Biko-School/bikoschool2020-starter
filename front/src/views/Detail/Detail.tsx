f1df6fce46353d6588d03d1141fd399153007399
user <user@PRESTAMOA01.bikowifi>
2020-11-20 17:30:06 +0100

    Test muestra detalle del meme pasandole el id por parametro

--- front/src/views/Detail/Detail.tsx
+++ front/src/views/Detail/Detail.tsx
@@ -3,7 +3,11 @@ import React from 'react'
 export const Detail: React.FC = () => {
   return (
     <>
-      <p>Movie Brazil GIF by MOODMAN</p>
+      <h1>Movie Brazil GIF by MOODMAN</h1>
+      <img
+        src="https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif"
+        alt="Movie Brazil GIF by MOODMAN"
+      />
     </>
   )
 }
--- front/src/views/Detail/__tests__/Detail.test.tsx
+++ front/src/views/Detail/__tests__/Detail.test.tsx
@@ -1,13 +1,30 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { screen, render } from '@testing-library/react'
 import { Detail } from '../Detail'
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter, MemoryRouter, Route } from 'react-router-dom'
 
 describe('Detail of a meme', () => {
-  it('should show a meme title', async () => {
-    render(<Detail />, { wrapper: BrowserRouter })
+  it('should show the detail of the meme', async () => {
+    render(
+      <MemoryRouter initialEntries={['/memes/YleuWir5NTNVXkflSp']}>
+        <Route path="/memes/:id">
+          <Detail />
+        </Route>
+      </MemoryRouter>,
+    )
 
-    const testElement = await screen.findByText('Movie Brazil GIF by MOODMAN')
-    expect(testElement).toBeInTheDocument()
+    const memeTitleElement = await screen.findByText(
+      'Movie Brazil GIF by MOODMAN',
+    )
+    const memeImageElement = await screen.findByRole('img', {
+      name: 'Movie Brazil GIF by MOODMAN',
+    })
+
+    expect(memeTitleElement).toBeInTheDocument()
+    expect(memeImageElement).toBeInTheDocument()
+    expect(memeImageElement).toHaveAttribute(
+      'src',
+      'https://media4.giphy.com/media/YleuWir5NTNVXkflSp/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif',
+    )
   })
 })
