ce72ff72c74af4025d2a152bb52ddb95bb5a8654
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-11-20 18:58:07 +0100

    test: delete should can view meme details when click on meme

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, screen } from '@testing-library/react'
+import { render, screen, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { server } from './mocks/server'
 import { rest } from 'msw'
@@ -7,7 +7,7 @@ import App from './App'
 import { Meme } from './models/Meme'
 import { memes } from './fixture/recent.json'
 import searchResultMovie from './fixture/search_movie.json'
-import { renderWithProviders } from './testUtils'
+import { renderWithProviders, history } from './testUtils'
 
 describe('App', () => {
   it('Should render home page ', async () => {
@@ -16,22 +16,5 @@ describe('App', () => {
     expect(
       await screen.findByText(/Los guif mas trending del momento/i),
     ).toBeInTheDocument()
-  })
-  it('should can view meme details when click on meme', async () => {
-    const meme: Meme = memes[0]
-
-    server.use(
-      rest.get('http://localhost:5000/api/memes', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ memes: [meme] }))
-      }),
-    )
-    renderWithProviders(<App />)
-
-    userEvent.click(
-      await screen.findByRole('img', {
-        name: meme.title,
-      }),
-    )
-    expect(await screen.findByText(meme.title)).toBeInTheDocument()
   })
 })
