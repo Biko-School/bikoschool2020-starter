ddbdb67967cdee979d6e49dd9696f0bd5c44a914
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-11-21 13:40:02 +0100

    refactor: delete duplicated test

--- front/src/views/Home/Home.test.tsx
+++ front/src/views/Home/Home.test.tsx
@@ -58,35 +58,6 @@ describe('Listado de memes', () => {
       await screen.findByText(memeWithAuthor.author?.displayName as string),
     ).toBeInTheDocument()
   })
-  it('should show author when meme has this info', async () => {
-    const memeWithAuthor: Meme = {
-      id: 'YleuWir5NTNVXkflSp',
-      title: 'Movie Brazil GIF by MOODMAN',
-      url:
-        'https://media4.giphy.com/media/YleuWir5NTNVXkflSp/200w.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=200w.gif',
-      author: {
-        displayName: 'Author Name',
-        avatarUrl: 'avatar.jpg',
-      },
-    }
-    server.use(
-      rest.get('http://localhost:5000/api/memes', (req, res, ctx) => {
-        return res(ctx.status(200), ctx.json({ memes: [memeWithAuthor] }))
-      }),
-    )
-    renderWithProviders(<Home />)
-
-    const authorAvatar = await screen.findByRole('img', {
-      name: memeWithAuthor.author?.displayName,
-    })
-    expect(authorAvatar).toHaveAttribute(
-      'src',
-      memeWithAuthor.author?.avatarUrl,
-    )
-    expect(
-      await screen.findByText(memeWithAuthor.author?.displayName as string),
-    ).toBeInTheDocument()
-  })
 })
 
 describe('Search memesSearch memes', () => {
