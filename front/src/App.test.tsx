ae28f53d1abbca3d69773d43bef1f7a191508a89
Leyre <leyre.garralda@gmail.com>
2020-11-21 13:14:35 +0100

    refactor: actualiza los tests para corregir unmounted component error

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import App from './App'
-import Memes from './memes.json'
+import Memes from '../fixtures/dbSampleMemes.json'
 import { server } from './mocks/server'
 import { rest } from 'msw'
 
@@ -44,70 +44,63 @@ describe('listado de memes', () => {
 })
 
 describe('busqueda de memes', () => {
-  test.skip('muestra un campo de busqueda y un botón', async () => {
-    render(<App />)
-
-    await screen.findByRole('img', { name: 'Funny Gif Lol GIF by MOODMAN' })
-    expect(screen.findByRole('textbox')).toBeInTheDocument()
-    expect(screen.findByRole('button')).toBeInTheDocument()
-  })
-
-  test.skip('llama a la api con el texto buscado', async () => {
+  test('llama a la api con el texto buscado', async () => {
     const fetch = jest.spyOn(window, 'fetch')
     render(<App />)
-    const textbox: HTMLInputElement = screen.getByRole(
-      'textbox',
-    ) as HTMLInputElement
 
-    const button = screen.getByRole('button')
+    const searchBox = screen.getByRole('textbox', { name: /buscar/i })
+    userEvent.type(searchBox, 'lol')
+    expect(searchBox).toHaveValue('lol')
 
-    // fireEvent.change(textbox, { target: { value: 'lol' } })
-    userEvent.type(textbox, 'lol')
-    expect(textbox.value).toBe('lol')
-
-    // fireEvent.click(button)
-    userEvent.click(button)
+    const searchButton = screen.getByRole('button', { name: /búsqueda/i })
+    userEvent.click(searchButton)
 
     expect(fetch).toBeCalledWith(
       process.env.REACT_APP_API_BASE_URL + '/memes?search=lol',
     )
+
+    await screen.findByRole('img', {
+      name: /Season 2 Slow Clap GIF by CBS All Access/i,
+    })
   })
 
-  test.skip('si la longitud de la busqueda es menor de 3 da error', async () => {
+  test('si la longitud de la busqueda es menor de 3 da error', async () => {
     const fetch = jest.spyOn(window, 'fetch')
     render(<App />)
-    const textbox: HTMLInputElement = screen.getByRole(
-      'textbox',
-    ) as HTMLInputElement
+    await screen.findByRole('img', {
+      name: /Brazil/i,
+    })
+    const searchBox = screen.getByRole('textbox')
 
-    const button = screen.getByRole('button')
+    const searchButton = screen.getByRole('button')
 
-    // fireEvent.change(textbox, { target: { value: 'lo' } })
-    // fireEvent.click(button)
-    userEvent.type(textbox, 'lo')
-    userEvent.click(button)
+    userEvent.type(searchBox, 'lo')
+    userEvent.click(searchButton)
 
-    expect(fetch).not.toBeCalledWith(
-      process.env.REACT_APP_API_BASE_URL + '/memes?search=' + textbox.value,
-    )
+    await waitFor(() => {
+      expect(fetch).not.toBeCalledWith(
+        process.env.REACT_APP_API_BASE_URL + '/memes?search=' + 'lo',
+      )
+    })
   })
 
   test('muestra los memes que devuelve la busqueda', async () => {
     render(<App />)
-    const textbox: HTMLInputElement = screen.getByRole(
-      'textbox',
-    ) as HTMLInputElement
-    const button = screen.getByRole('button')
+    const searchBox: HTMLInputElement = screen.getByRole('textbox', {
+      name: /buscar/i,
+    }) as HTMLInputElement
+    const searchButton = screen.getByRole('button', { name: /búsqueda/i })
 
     await screen.findAllByRole('img')
 
-    userEvent.type(textbox, 'lol')
-    userEvent.click(button)
+    userEvent.type(searchBox, 'lol')
+    userEvent.click(searchButton)
 
-    // screen.debug()
     await waitFor(async () => {
-      const memes = await screen.findAllByRole('img')
-      expect(memes).toHaveLength(2)
+      const memes = await screen.findAllByRole('img', {
+        name: /Season 2 Slow Clap GIF by CBS All Access/i,
+      })
+      expect(memes).toHaveLength(1)
     })
   })
 })
--- front/src/mocks/handlers.ts
+++ front/src/mocks/handlers.ts
@@ -1,13 +1,14 @@
 // src/mocks/handlers.js
 import { rest } from 'msw'
-import Memes from '../memes.json'
+import Memes from '../../fixtures/dbSampleMemes.json'
+import SearchMemes from '../../fixtures/dbMemesSearchResult.json'
 
 export const handlers = [
   rest.get(process.env.REACT_APP_API_BASE_URL + '/memes', (req, res, ctx) => {
     const search = req.url.searchParams.get('search')
 
     if (search) {
-      return res(ctx.status(200), ctx.json([Memes[0], Memes[1]]))
+      return res(ctx.status(200), ctx.json(SearchMemes))
     }
     //Return a mocked memes list
     return res(ctx.status(200), ctx.json(Memes))
