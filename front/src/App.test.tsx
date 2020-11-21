a07ab8af31e4ffef75a49578ae46d63803bf94a7
miriam-gaviria2 <52879306+miriam-gaviria2@users.noreply.github.com>
2020-11-21 09:47:51 +0100

    test: Should render meme details

--- front/src/App.test.tsx
+++ front/src/App.test.tsx
@@ -8,6 +8,7 @@ import { Meme } from './models/Meme'
 import { memes } from './fixture/recent.json'
 import searchResultMovie from './fixture/search_movie.json'
 import { renderWithProviders, history } from './testUtils'
+import { MemeDetails } from './models/MemeDetails'
 
 describe('App', () => {
   it('Should render home page ', async () => {
@@ -15,6 +16,33 @@ describe('App', () => {
 
     expect(
       await screen.findByText(/Los guif mas trending del momento/i),
+    ).toBeInTheDocument()
+  })
+  it('Should render meme details ', async () => {
+    const meme: MemeDetails = {
+      id: '26xBKlTBLN10xp7oc',
+      title: 'Stranger Things Reaction GIF',
+      url:
+        'https://media3.giphy.com/media/26xBKlTBLN10xp7oc/giphy.gif?cid=be655fb7f245f7d29df0fc743b70e3ee884dbaf31956e789&rid=giphy.gif',
+      tags: ['#movie'],
+      author: {
+        displayName: 'SAG Awards',
+        avatarUrl:
+          'https://media4.giphy.com/avatars/sagawards/HsvQ4GGcXMxE.jpg',
+      },
+    }
+    server.use(
+      rest.get('http://localhost:5000/api/memes/:id', (_, res, ctx) =>
+        res(ctx.status(200), ctx.json(meme)),
+      ),
+    )
+    renderWithProviders(<App />, { route: '/meme/id123' })
+
+    expect(
+      await screen.findByRole('heading', {
+        name: `${meme.title} by ${meme.author?.displayName}`,
+        level: 1,
+      }),
     ).toBeInTheDocument()
   })
 })
