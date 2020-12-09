9485377c93b6006f8a6f64cf84646cd322e96012
Jesús Olazagoitia <jesus.designer@gmail.com>
2020-12-09 19:24:57 +0100

    refactor: test searchbox

--- front/src/views/_components/SearchBox/SearchBox.test.tsx
+++ front/src/views/_components/SearchBox/SearchBox.test.tsx
@@ -3,28 +3,39 @@ import { render, screen, act } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import { SearchBox } from './SearchBox'
 
+const mockHistoryPush = jest.fn()
+jest.mock('react-router-dom', () => ({
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
 describe('Search Box', () => {
   it('should search if user presses enter', () => {
-    const onSearch = jest.fn()
-    render(<SearchBox onSearch={onSearch} />)
+    render(<SearchBox />)
 
     userEvent.type(screen.getByRole('searchbox'), 'movie{enter}')
 
-    expect(onSearch).toBeCalledWith('movie')
+    expect(mockHistoryPush).toBeCalledWith('/search/movie')
   })
 
   it('search button should be disabled when search term has less than three characters', () => {
-    const onSearch = jest.fn()
-    render(<SearchBox onSearch={onSearch} />)
+    render(<SearchBox />)
     const button = screen.getByRole('button', { name: 'Search' })
     const searchBox = screen.getByRole('searchbox')
-
-    expect(button).toHaveAttribute('disabled')
 
     userEvent.type(searchBox, '12')
     expect(button).toHaveAttribute('disabled')
 
     userEvent.type(searchBox, '3')
     expect(button).not.toHaveAttribute('disabled')
+  })
+
+  it('when empty search should redirect to home page', () => {
+    render(<SearchBox />)
+
+    userEvent.click(screen.getByRole('button', { name: 'Search' }))
+
+    expect(mockHistoryPush).toBeCalledWith('/')
   })
 })
